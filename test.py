5ce62f3801c2ecce26bedae25cd06553df4555c2
HKishere <939132879@qq.com>
2019-08-14 18:03:52 +0800

    create the code

---
+++ test.py
@@ -0,0 +1,8 @@
+a = [1,2,3]
+b = [1,2,3]
+
+if id(a) == id(b):
+    print('kishere')
+else:
+    print("dd")
+    
---
+++ 猜字游戏.py
@@ -0,0 +1,8 @@
+print('==============我的第一个pyhon游戏================')
+temp = input('please enter a number you guess:')
+guess = int(temp)
+if guess==34:
+      print('you are great!')
+else:
+      print('you are not good enough')
+print('game over')
