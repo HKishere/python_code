3ee78f9516f4cf4286814d48cba6255aab44bcde
HKishere <939132879@qq.com>
2020-02-05 01:28:04 +0800

    开工

--- sudu.py
+++ sudu.py
@@ -1,7 +1,33 @@
-def init():
-    map = []
-    for each in range(1,9):
-        buf = input("input the sudu value, use 0 to replace the blank:")
-        list(buf)
-        map.append(buf)
+map = []
+def Init():
+    for each in range(1,10):
+        print('input the sudu value of %dth row, use 0 to replace the blank:' %each)
+        buf = input()
+        while len(buf) != 9:
+            print('input the sudu value of %dth row, use 0 to replace the blank:' %each)
+            buf = input()
+        else:
+            map.append(buf)
+    
+    print("here is your map:\n==========")
+    
+    for each in map:
+        print(each)
+def FindMaxNum():
+    pass
 
+
+def Solve(row, col):
+    if (col < 9):
+        if (row < 9):
+            if(map[row][col] == '0'):
+
+
+
+
+def Run():
+    Init()
+    Solve(1,2)
+
+if __name__ == "__main__":
+    Run()
