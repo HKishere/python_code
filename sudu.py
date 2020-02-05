d1f602a896ff889258f4c49e99049c5fd9f3c3de
HKishere <939132879@qq.com>
2020-02-05 15:45:23 +0800

    完善代码

--- sudu.py
+++ sudu.py
@@ -13,21 +13,38 @@ def Init():
     
     for each in map:
         print(each)
-def FindMaxNum():
-    pass
+
+def blockcheck(row, col):
+    start_point = map[row//3][col//3]
+    for each in (0,3):
 
 
 def Solve(row, col):
-    if (col < 9):
-        if (row < 9):
-            if(map[row][col] == '0'):
-
+    if (row < 9) & (col < 9):
+        if map[row][col] == '0':
+            for each in range(0,9):
+                map_row = map[row]
+                map_col = ''
+                for each in range(0,9):
+                    map_col.join(map[each][col])
+                if (map_row.find(str(each)) == -1) and (map_col.find(str(each)) == -1):
+                    map[row][col] = str(each)
+                    if Solve(row, col + 1) == 1:
+                        return 1
+                else:
+                    return 0
+    else:
+        if row < 8:
+            Solve(row + 1, 0)
+        else:
+            return 1
+            
 
 
 
 def Run():
     Init()
-    Solve(1,2)
+    Solve(0,0)
 
 if __name__ == "__main__":
     Run()
