d84c53f5e8e78dacf36fe847771ad63f7a34f9ae
HKishere <939132879@qq.com>
2020-02-19 19:25:13 +0800

    放开自动关机代码

--- yiban_auto_fill_for_PC.py
+++ yiban_auto_fill_for_PC.py
@@ -1,7 +1,17 @@
 import uiautomator2 as ut2
 import os
+import subprocess
 import time
-d = ut2.connect('127.0.0.1:62025')
+try:
+    d = ut2.connect('127.0.0.1:62025')
+    print('模拟器启动,连接成功')
+except RuntimeError as reason:
+    print('看样子模拟器没启动'+str(reason))
+    print('启动模拟器')
+    run_simulator = subprocess.Popen(r"Nox.exe -clone:Nox_1")
+    d = ut2.connect('127.0.0.1:62025')
+    print('模拟器启动,连接成功')
+
 def Init():
     if (d.info.get('screenOn') == False):
         print("屏幕关闭,打开屏幕")
@@ -17,10 +27,10 @@ def Init():
     time.sleep(0.1)
     d.press("home")
 
-def Fill_Font():
+def Open_yiban():
     sess = d.session("com.yiban.app")
     print("进入易班")
-    time.sleep(10)
+    time.sleep(14)
     if d(resourceId="com.yiban.app:id/page_download_content").exists:
         d(resourceId="com.yiban.app:id/page_download_cancel").click()
     d(resourceId="com.yiban.app:id/tv_name", text="信息上报").click()
@@ -31,47 +41,54 @@ def Fill_Font():
         d(resourceId="com.yiban.app:id/tv_name", text="信息上报").click()
         time.sleep(8)
     print("进入填写页面")
-    time.sleep(2)
-    d.click(0.456, 0.557)
-    d.click(0.456, 0.557)
-    time.sleep(1)
-    d.swipe(0.547, 0.317, 0.536, 0.119,0.2)
-    d.set_fastinput_ime(True)
-    d.click(0.382, 0.822)
-    d.send_keys("黄祚恺")
-    d.click(0.412, 0.068)
-    #选辅导员
-    d.swipe(0.547, 0.900, 0.536, 0.119,0.4)
-    d.click(0.29, 0.17)
-    time.sleep(1)
-    d.swipe(0.378, 0.922, 0.364, 0.733,0.2)
-    d.swipe(0.73, 0.824, 0.72, 0.857,0.2)
-    time.sleep(1)
-    d.swipe(0.729, 0.88, 0.726, 0.629,0.2)
-    d.click(0.937, 0.675)
-    #填下一个(是否无变化)
-    d.swipe(0.468, 0.914, 0.472, 0.545,0.3)
-    time.sleep(1)
-    d.click(0.444, 0.654)
-    time.sleep(1)
-    d.click(0.94, 0.738)
-    time.sleep(1)
-    #点击承诺书
-    d.click(0.108, 0.719)
-    time.sleep(1)
-    d.click(0.534, 0.329)
-    time.sleep(1)
-    d.click(0.513, 0.807)
-    print("填写完成")
 
+def Auto_Fill():
+    time.sleep(2)
+    d.click(0.488, 0.5)
+    d.click(0.488, 0.5)
+
+    d.click(0.456, 0.917)
+    d.set_fastinput_ime(True)
+    d.click(0.454, 0.92)
+    d.send_keys("黄祚恺")
+    #d.set_fastinput_ime(False)
+
+    d.click(0.990, 0.68)
+    time.sleep(0.5)
+    d.swipe(0.465, 0.590, 0.465, 0.140,0.4)
+    time.sleep(0.7)
+    d.swipe(0.465, 0.690, 0.465, 0.080,0.4)
+    time.sleep(0.7)
+    d.click(0.278, 0.289)
+    time.sleep(0.7)
+    d.swipe(0.385, 0.966, 0.379, 0.780,0.2)
+    d.swipe(0.758, 0.851, 0.755, 0.860,0.1)
+    time.sleep(0.5)
+    d.swipe(0.787, 0.951, 0.787, 0.670,0.2)
+    d.click(0.698, 0.572)
+    time.sleep(0.7)
+
+    d.swipe(0.965, 0.990, 0.965, 0.500,0.2)
+    d.click(0.344, 0.687)
+    d.click(0.551, 0.618)
+    time.sleep(0.5)
+    d.click(0.089, 0.752)
+    time.sleep(0.5)
+    d.click(0.701, 0.273)
+    time.sleep(0.5)
+    d.click(0.666, 0.836)
+    print("填写完成")
 
 def main():
     clock = time.localtime()
-    while clock.tm_hour != 1:
+    while (clock.tm_hour != 1):
+        print("时候未到")
         clock =time.localtime()
         time.sleep(60)
     Init()
-    Fill_Font()
+    Open_yiban()
+    Auto_Fill()
+    print("准备关机")
     os.system('shutdown -s -f -t 59')
 
 
